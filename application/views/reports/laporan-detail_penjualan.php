4fb9e89c2d00cefe1c4536e30d5ea9c4a16dd575
Orlando Dimas Saputra <148965896+alwaysdims@users.noreply.github.com>
2026-01-20 13:21:37 +0700

    ubah nota

--- application/views/reports/laporan-detail_penjualan.php
+++ application/views/reports/laporan-detail_penjualan.php
@@ -76,7 +76,7 @@
         /* Watermark Image */
         .watermark-logo {
             position: absolute;
-            top: 35%;
+            top: 30%;
             left: 50%;
             transform: translate(-50%, -50%);
             width: 300px;
